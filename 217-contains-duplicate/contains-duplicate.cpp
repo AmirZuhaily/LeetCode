599e1ba6441f78113b9b492989765743bb13c679
Amir Zuhaily Bin Zaily <101566193+AmirZuhaily@users.noreply.github.com>
2026-07-11 19:38:27 +0800

    Time: 71 ms (25.95%) | Memory: 111.2 MB (15.51%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> counts;
+        for(int num : nums){
+            if(counts.count(num)){
+                return true;
+            }
+            counts.insert(num);
+        }
+        return false;
+    }
+};
